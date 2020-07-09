e5446efa8bb1491d3b9d73803f4ba3f62630771d
EC2 Default User <ec2-user@ip-172-31-11-49.us-west-1.compute.internal>
2020-07-09 00:52:14 +0000

    added Card class

--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
 $(document).ready(function() {
     
-    var numCards = 52;
-    var handSize = 5;
-    var holds = [false, false, false, false, false];
+    var numCards = 52; //number of cards in deck
+    var handSize = 5; //number of cards in a hand
+    var hand;
+    var coins = 100; //number of coins in purse
+    
+    var holds = [false, false, false, false, false]; // is card index held
     var cards = ["2C.gif", "2D.gif", "2H.gif", "2S.gif",
                 "3C.gif", "3D.gif", "3H.gif", "3S.gif",
                 "4C.gif", "4D.gif", "4H.gif", "4S.gif",
@@ -17,7 +20,15 @@ $(document).ready(function() {
                 "KC.gif", "KD.gif", "KH.gif", "KS.gif",
                 "AC.gif", "AD.gif", "AH.gif", "AS.gif"];
     
-    $("#draw").on("click", draw); 
+    $("#draw").on("click", draw);
+    $("#card1").on( "click", function() {
+        if (holds[0] == false) {
+            setHold(index, true);
+        }
+        else {
+            setHold(index, false);
+        }
+    });
     
     function draw() {
         for(let i = 1; i <= handSize; i++) {
@@ -31,4 +42,89 @@ $(document).ready(function() {
     function setHold(index, bool) {
         holds[index] = bool;
     }
+    
+    function jackOrBetter() {
+        
+    }
 }); //ready
+
+class Card {
+    
+    // suit enum
+    suit = Object.freeze({
+        CLUBS: "clubs",
+        DIAMONDS: "diamonds",
+        HEARTS: "hearts",
+        SPADES: "spades"
+    });
+    
+    __value; //card's face value
+    __suit; //cards suit
+    __errorFlag; //is card legal
+    
+    constructor(suit, value) {
+        set(suit, value);
+    }
+    
+    set setSuit(suit) {
+        if (isValid(value, suit)) {
+            this.__suit = suit;
+            __errorFlag = false;
+            return __errorFlag;
+        }
+        this.__suit = suit;
+        __errorFlag = true;
+        return __errorFlag;
+    }
+    
+    set setValue(value) {
+        if (isValid(value, suit)) {
+            this.__value = value;
+            __errorFlag = false;
+            return __errorFlag;
+        }
+        this.__value = value;
+        __errorFlag = true;
+        return __errorFlag;
+    }
+    
+    isValid(suit, value) {
+        if (__value == 'A' || __value == '2' || __value == '3' || __value == '4' || 
+            __value == '5' || __value == '6' || __value == '7' || __value == '8' ||
+            __value == '9' || __value == 'T' || __value == 'J' || __value == 'Q' ||
+            __value == 'K') 
+        {
+            return true;
+        } 
+        return false;
+    }
+    
+    equals(card) {
+        if (card != null && card != undefined && card.__suit == this.__suit
+            && card.__value == this.__value) {
+            return true;
+        }
+        return false;
+    }
+    
+    get getValue() {
+        return __value;
+    }
+    
+    get getSuit() {
+        return __suit;
+    }
+    
+    get getErrorFlag() {
+        return __errorFlag;
+    }
+}
+
+class Hand {
+    static NUM_CARDS = 5; //cards per hand
+    
+    constructor() {
+        
+    }
+    
+}
