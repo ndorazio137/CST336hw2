78c1021fc49ccd71235278fd28f064e47d45f16f
EC2 Default User <ec2-user@ip-172-31-11-49.us-west-1.compute.internal>
2020-07-12 08:21:30 +0000

    added sort (needs to be fixed), separated displayCards for reuse, made ranks an array of objects, updated cards with a key.

--- js/script.js
+++ js/script.js
@@ -1,25 +1,32 @@
 $(document).ready(function() {
 
-    //event listeners.
-    $("#draw").on("click", draw);
-    
-    $("#card0").on("click", holdCard);
-    $("#card1").on("click", holdCard);
-    $("#card2").on("click", holdCard);
-    $("#card3").on("click", holdCard);
-    $("#card4").on("click", holdCard);
-
     //possible states of gameplay
     var gameState = Object.freeze({
-        DEAL: 0,
-        DRAW: 1,
-        WIN: 2,
-        LOSE: 3,
-        GAMEOVER: 4
+        INTIALIZE: 0,
+        DEAL: 1,
+        DRAW: 2,
+        WIN: 3,
+        LOSE: 4,
+        GAMEOVER: 5
     });
 
     //card values.
-    var ranks = ['2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A'];
+    //var ranks = ['2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A'];
+    var ranks = [{key: 1, value: 'A'},
+                       {key: 2, value: '2'},
+                       {key: 3, value: '3'},
+                       {key: 4, value: '4'},
+                       {key: 5, value: '5'},
+                       {key: 6, value: '6'},
+                       {key: 7, value: '7'},
+                       {key: 8, value: '8'},
+                       {key: 9, value: '9'},
+                       {key: 10, value: 'T'},
+                       {key: 11, value: 'J'},
+                       {key: 12, value: 'Q'},
+                       {key: 13, value: 'K'},
+                       {key: 14, value: 'A'}];
+    
     //card suits.
     var suits = ['C', 'D', 'H', 'S'];
 
@@ -30,106 +37,114 @@ $(document).ready(function() {
     var bet = 0;
     var previouslyBetted = false;
     var previousBet = 0;
-    var __currentGameState = gameState.DEAL;
+    var __currentGameState = gameState.INTIALIZE;
 
-    (function game() {
-        if (__currentGameState == gameState.DEAL) {
-            initialize();
-            $("#bet-one").on("click", betOne);
-            $("#bet-max").on("click", betMax);
-            $("#increase-bet").on("click", increaseBet);
-            $("#decrease-bet").on("click", decreaseBet);
-        }
-
-        if (__currentGameState == gameState.DRAW) {
-            draw();
-        }
-
-        if (__currentGameState == gameState.WIN) {
-
-        }
-
-        if (__currentGameState == gameState.LOSE) {
-
-        }
-
-        if (__currentGameState == gameState.GAMEOVER) {
-
-        }
-    })();
-
-    function initialize() {
+    (function initialize() {
+        __currentGameState = gameState.DEAL;
         deck = allocateDeck();
         hand = allocateHand(__handSize);
         addCardsToDeck();
         shuffle(deck.cards);
         dealCards();
-        __currentGameState = gameState.DRAW;
-    }
-
-    function addCardsToDeck() {
-        ranks.forEach((rank) => {
-            suits.forEach((suit) => {
-                let card = new Card(suit, rank);
-                deck.cards.push(card);
-            });
+        displayCards();
+        
+        // Enable all buttons.
+        $("#bet-one").on("click", betOne);
+        $("#bet-max").on("click", betMax);
+        $("#increase-bet").on("click", increaseBet);
+        $("#decrease-bet").on("click", decreaseBet);
+        $("#card0").on("click", holdCard);
+        $("#card1").on("click", holdCard);
+        $("#card2").on("click", holdCard);
+        $("#card3").on("click", holdCard);
+        $("#card4").on("click", holdCard);
+        
+        $("#draw").click(function(){
+            __currentGameState = gameState.DRAW;
+             draw();
         });
-    }
-
-    // Deals the cards and updates the hand
-    function dealCards() {
-        // fill hand
-        let i;
-        for (i = 0; i < hand.cards.length; i++) {
-            hand.cards[i] = deck.cards.pop();
-            // Display hand to screen
-            $(`#card${i}`).attr("src", "img/" + hand.cards[i].value +
-                hand.cards[i].suit + ".gif");
+    })();
+    
+    //TODO: update for held cards
+    function draw() {
+        if (gameState = gameState.DRAW) {
+            // Disable all buttons except for "Draw".
+            $("#draw").prop("disabled", true);
+            $("#bet-one").prop("disabled", true);
+            $("#bet-max").prop("disabled", true);
+            $("#increase-bet").prop("disabled", true);
+            $("#decrease-bet").prop("disabled", true);
+            $("#card0").prop("disabled", true);
+            $("#card1").prop("disabled", true);
+            $("#card2").prop("disabled", true);
+            $("#card3").prop("disabled", true);
+            $("#card4").prop("disabled", true);
+            dealCards();
+            sort(hand);
+            displayCards();
         }
     }
 
-    //Display held card text
-    function holdCard(card) {
-        card.held = !card.held;
+    function addCardsToDeck() {
+        let i;
+        //starts at 1 to avoid duplicate Aces.
+        for (i = 1; i < ranks.length; i++) {
+            suits.forEach((suit) => {
+                let card = new Card(suit, ranks[i].value, ranks[i].key);
+                deck.cards.push(card);
+            });
+        }
     }
 
-    //TODO: update for held cards
-    function draw() {
-        dealCards();
+    // Deals cards to the hand
+    function dealCards() {
+        let i;
+        for (i = 0; i < hand.cards.length; i++) {
+            hand.cards[i] = deck.cards.pop(); 
+        }
+    }
+    
+    // Updates cards on screen.
+    function displayCards() {
+        for (i = 0; i < hand.cards.length; i++) {
+            $(`#card${i}`).attr("src", "img/" + hand.cards[i].value +
+                    hand.cards[i].suit + ".gif");
+        }
     }
 
-    //TODO: revert if bet max was pressed first
+    // Bet one button was pressed.
     function betOne() {
         if (previouslyBetted == true) {
             purse = purse + previousBet;
         }
-        
-        if(purse > 0) {
+
+        if (purse > 0) {
             bet = 1;
             --purse;
             previousBet = bet;
             previouslyBetted = true;
         }
-            
+
         displayBet();
     }
 
-    //TODO: revert if bet one was pressed first
+    // Bet max button was pressed.
     function betMax() {
         if (previouslyBetted == true) {
             purse = purse + previousBet;
         }
-            
+
         if (purse >= 5) {
             bet = 5;
             purse = purse - 5;
             previousBet = bet;
             previouslyBetted = true;
         }
-        
+
         displayBet();
     }
 
+    // "+"/Increase Bet button was pressed.
     function increaseBet() {
         if ((purse >= 0) && (bet < 5)) {
             ++bet;
@@ -137,34 +152,55 @@ $(document).ready(function() {
             --purse;
             previouslyBetted = true;
         }
-        
+
         displayBet();
     }
 
+    // "-"/Decrease Bet button was pressed. 
     function decreaseBet() {
+        if (bet == 0) {
+            ++bet;
+            --purse;
+        }
+
         if (bet > 1) {
             --bet;
             --previousBet;
             ++purse
             previouslyBetted = true;
         }
-        
+
         displayBet();
     }
 
+    // Displays Purse and Bet text to screen.
     function displayBet() {
         document.getElementById("purse-info").innerHTML = "PURSE: " + purse.toString() + " COINS";
         document.getElementById("bet-info").innerHTML = "BET: " + bet.toString() + " COINS";
         document.getElementById("bet-text").innerHTML = bet.toString();
     }
+    
+    
+    //TODO: finish.
+    function holdCard() {
+        
+    }
+    
+    // check for winning hands.
+    function checkForWin(array) {
+       sort(array);
+       
+    }
+
 
 }); //ready
 
 // Creates a card. 
-function Card(suit, value) {
+function Card(suit, value, key) {
     let card = {
         value: value,
         suit: suit,
+        key: key,
         held: true
     };
 
@@ -189,7 +225,7 @@ function allocateDeck() {
     return deck;
 }
 
-// Shuffles any array.
+// Fisher-Yates shuffles. Used to shuffle a deck.
 function shuffle(array) {
     let i, temp, random;
     for (i = 0; i < array.length; i++) {
@@ -199,3 +235,22 @@ function shuffle(array) {
         array[random] = temp;
     }
 }
+
+
+//TODO: Fix this.
+// Simple insertion sort. Used to sort the hand to simplify checking winning hands.
+function sort(array) {
+    let i; 
+    for (i = 1; array.length; i++) {
+       let key = array[i];
+       let j = i - 1;
+       while (j >= 0 && array[j].key < array[j].key) {
+           array[j + 1] = array[j];
+           --j;
+       }
+       array[j + 1] = key;
+    }
+}
+
+
+
